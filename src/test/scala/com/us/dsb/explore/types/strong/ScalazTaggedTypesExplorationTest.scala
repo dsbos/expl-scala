df3b1dc042da5a46dea61038564996f1e38ab274
Daniel Barclay <dbarclay@savi.com>
2017-10-29 11:37:41 -0400

    Tagged types expl.:  Import scalaz.Tag, etc.; renamed my types.

--- src/test/scala/com/us/dsb/explore/types/strong/ScalazTaggedTypesExplorationTest.scala
+++ src/test/scala/com/us/dsb/explore/types/strong/ScalazTaggedTypesExplorationTest.scala
@@ -3,6 +3,8 @@ package com.us.dsb.explore.types.strong
 import org.scalatest.FunSuite
 import org.scalatest.Matchers._
 
+import scalaz.Tag
+import scalaz.Tags
 import scalaz.@@   // Note: Definition depends on ScalaZ version.
 
 
@@ -10,31 +12,63 @@ class ScalazTaggedTypesExplorationTest extends FunSuite {
 
   test("Tagged ...") {
 
-    trait TypeA
-    trait TypeB
+    trait ThingA
+    trait ThingB
 
-    type TypeAInt = Int @@ TypeA
-    type TypeBInt = Int @@ TypeB
+    type ThingAInt = Int @@ ThingA
+    type ThingBInt = Int @@ ThingB
 
-    ////////////////////////////////////////
+
+    // assim: Tag(...) and other shorter versions of ".asInstanceOf[...]"
+
+    /** Like regular case class companion object and apply(...). */
+    object ThingAInt {
+      def apply(i: Int): ThingAInt = i.asInstanceOf[ThingAInt]
+    }
+    implicit class IntToThingAInt(value: Int) /* extends AnyVal (but not nestable here) */ {
+      def thingA: ThingAInt = value.asInstanceOf[ThingAInt]
+    }
+
+    val x1: ThingAInt = 1.asInstanceOf[ThingAInt]
+
+    val x15: ThingAInt = ThingAInt(15)
+    val x16: ThingAInt = 1.thingA
+
+    val x2: ThingAInt = Tag(2)
+
+    val x3 = Tag(3)             // bad: gets @@[Int, Nothing]
+
+    val x4a = Tag[Int, ThingA](3)
+    val x4b: ThingAInt = x4a
+
+    val t1 = Tag.of[ThingA](1)
+    val t2 = Tag.of[ThingAInt](x1)
+    val t3 = Tag.unwrap(x1)
+
+    Tag.unwrap(x1) + 1
+
+
+    ////////////////////
     // Strong typing at simple assignment:
-    val a1: TypeAInt = 1.asInstanceOf[TypeAInt]
-    val a1b: TypeAInt = a1
-    "val b1: TypeBInt = a1" shouldNot compile
-    "val a2: TypeAInt = 2" shouldNot compile
+    val a1: ThingAInt = 1.asInstanceOf[ThingAInt]
+    val a1b: ThingAInt = a1
+    "val b1: ThingBInt = a1" shouldNot compile
+    "val a2: ThingAInt = 2" shouldNot compile
 
     ////////////////////
     // Seemingly good strong typing in unconstrained expression ...
-    val b2 = 2.asInstanceOf[TypeBInt]
+    val b2 = 2.asInstanceOf[ThingBInt]
     "a1 - b2" shouldNot compile
 
     ////////////////////
     // ... but loses access to "base" type's methods (broken relative to what
     // I wanted--unmixable versions of types still having normal operations as
     // in VHDL and Ada integer types):
-    "val a0: TypeAInt = a1 - a1" shouldNot compile // well, _doesn't, but should
-    "a1 - a1" shouldNot compile // well, _doesn't, but should
-    // yield "value - is not a member of TypeAInt"
+    "val a0: ThingAInt = a1 - a1" shouldNot compile // well, _doesn't_, but should
+    "a1 - a1" shouldNot compile // well, _doesn't_, but should
+    // yield "value - is not a member of ThingAInt"
+
+    // ???? add getting value out
 
     ////////////////////
     // Seemingly good strong typing in <what kind?> expression:
