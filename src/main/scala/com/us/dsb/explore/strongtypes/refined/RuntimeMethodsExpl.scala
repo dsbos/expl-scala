7ab80977290b5095784c4f397a8762d5695fd99c
Daniel Barclay <daniel.barclay@qomplx.com>
2021-12-30 22:07:42 -0500

    Simplified, clarified RuntimeMethodsExpl.

--- src/main/scala/com/us/dsb/explore/strongtypes/refined/RuntimeMethodsExpl.scala
+++ src/main/scala/com/us/dsb/explore/strongtypes/refined/RuntimeMethodsExpl.scala
@@ -3,38 +3,46 @@ package com.us.dsb.explore.strongtypes.refined
 import scala.util.Try
 
 object RuntimeMethodsExpl extends App {
-
   import eu.timepit.refined.api.Refined
-
+  import eu.timepit.refined.numeric.Positive
+  import eu.timepit.refined.types.numeric.PosInt
 
   import eu.timepit.refined.api.RefType
-  import eu.timepit.refined.collection.NonEmpty
+  import eu.timepit.refined.api.RefType.applyRef
   import eu.timepit.refined.refineV
 
-  val goodPlainString = "<non-empty string>"
-  val badPlainString = ""
+  // 1: RefType's applyRef: takes whole refined type, returns Either:
+  val form1ResultGood = applyRef[Int Refined Positive](42)
+  val form1ResultBad  = applyRef[Int Refined Positive](-420)
+  val form1ResultBad2 = applyRef[PosInt](-420)  // form with type alias
 
-  // Note "String Refined NonEmpty" vs. just "NonEmpty":
-  // (return types: Either[String, Refined[String, NonEmpty]])
-  val check1ResultGood = RefType.applyRef[String Refined NonEmpty](goodPlainString)
-  val check1ResultBad = RefType.applyRef[String Refined NonEmpty](badPlainString)
-  val check2ResultGood = refineV[NonEmpty](goodPlainString)
-  val check2ResultBad = refineV[NonEmpty](badPlainString)
+  // 2: refined's refineV: takes refinement only, returns Either:
+  val form2ResultGood = refineV[Positive](42)
+  val form2ResultBad  = refineV[Positive](-420)
 
-  println("RuntimeMethods:")
-  println("check1ResultGood = " + check1ResultGood)
-  println("check2ResultBad = " + check2ResultBad)
-  println("check1ResultGood = " + check1ResultGood)
-  println("check2ResultBad = " + check2ResultBad)
+  // 3: refineV.unsafeFrom takes refinement only, returns value or throws IllegalArgumentException
+  val form3ResultGood = refineV[Positive].unsafeFrom(42)
+  val form3WrappedResultBad = Try { refineV[Positive].unsafeFrom(-420)}
 
-  val x1: Refined[String, NonEmpty] = refineV[NonEmpty].unsafeFrom(goodPlainString)
-  println("x1 = " + x1)
-  val x2 = Try {
-    refineV[NonEmpty].unsafeFrom(badPlainString)
-  } // IllegalArgumentException
-  println("x2 = " + x2)
+  println("form1ResultGood = " + form1ResultGood)
+  println("form1ResultBad  = " + form1ResultBad)
+  println("form1ResultGood = " + form1ResultGood)
+  println("form2ResultBad  = " + form2ResultBad)
+  println("form3ResultGood       = " + form3ResultGood)
+  println("form3WrappedResultBad = " + form3WrappedResultBad)
 
   // re which: see https://kwark.github.io/refined-in-practice/#38
-  //
+
+  // What are / what about:
+  // - RefType.applyRef's sibling(s):
+  //   - applyRefM
+  // - eu.timepit.refined.refineV's siblings:
+  //   - refineT
+  //   - refineMV
+  //   - refineMT
+  // - something's unsafeWrap
+  // - something's unwrap
+  // - something's unsafeRewrap
+  // - something's unsafeApply
 
 }
