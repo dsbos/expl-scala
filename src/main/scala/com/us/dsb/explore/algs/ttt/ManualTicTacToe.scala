b676ffece604431aaf646f92cf8d521815052319
Daniel Barclay <daniel.barclay@qomplx.com>
2022-01-17 11:28:03 -0500

    ManualTicTacToe:  Cleaned get-command loop.  Started command loop.

--- src/main/scala/com/us/dsb/explore/algs/ttt/ManualTicTacToe.scala
+++ src/main/scala/com/us/dsb/explore/algs/ttt/ManualTicTacToe.scala
@@ -1,45 +1,68 @@
 package com.us.dsb.explore.algs.ttt
 
-import org.jline.terminal.TerminalBuilder
+
+import cats.syntax.option._
+import cats.syntax.either._
+
+import scala.annotation.tailrec
 
 object ManualTicTacToe extends App {
 
 
-  // ?? clean type-safety (w/Enumeratum enum.?)
+  // ?? revisit (Enumeratum enum.?)
   object Command extends Enumeration {
     val Up, Down, Left, Right, Mark, Quit = Value
   }
   type Command = Command.Value
 
-
-  //type Command = String  // ?? clean type
-
-  def getCommand(): Command = {
-    // ?? clean looping (var, while, .isEmpty redundancy with None cases, explicit .get);
-    //   possibly loop by calling sel recursively, but isn't there better Scala way?
-
-    var command: Option[Command] = None
-    while (command.isEmpty) {
-      print("Enter command for <player>  (u(p), d(own), l(eft), r(rigjt), m(ark), q(uit): ")
-
-      // ?? clean embedded reference to stdid/console
-      val rawCmd = scala.io.StdIn.readLine()
-      command = {
-        import Command._
-        rawCmd match {
-          case "u" => Some(Up)
-          case "d" => Some(Down)
-          case "l" => Some(Left)
-          case "r" => Some(Right)
-          case "m" => Some(Mark)
-          case "q" => Some(Quit)
-          case _ => println(s"Invalid command input line: \"$rawCmd\""); None
-        }
-      }
-    }
-    command.get
+  object Player extends Enumeration {
+    val X, O = Value
   }
-  val command = getCommand()
-  println("command = " + command)
+
+  type Player = Player.Value
+
+  def parseCommand(rawCmd: String): Either[String, Command] = {
+    import Command._
+    rawCmd match {
+      case "u" => Up.asRight
+      case "d" => Down.asRight
+      case "l" => Left.asRight
+      case "r" => Right.asRight
+      case "m" => Mark.asRight
+      case "q" => Quit.asRight
+      case _ => s"Invalid command input line: \"$rawCmd\"".asLeft
+    }
+  }
+
+  @tailrec
+  def getCommand(player: Player): Command = {
+    // ?? clean looking more (was while mess., now recursive; is there better Scala way?
+
+    // ?? clean embedded reference to stdin/console and stdout
+    print(s"$player, enter command (u(p), d(own), l(eft), r(rigjt), m(ark), q(uit): ")
+    val rawCmd = scala.io.StdIn.readLine()
+
+    parseCommand(rawCmd) match {
+      case Right(cmd) => cmd
+      case Left(msg) =>
+        println(msg)
+        getCommand(player)
+    }
+  }
+
+  def doCommandLoop() = {
+
+    val command = getCommand(Player.X)
+    println("command = " + command)
+    import Command._
+
+    command match {
+      case Quit => "some result"
+      // doCommandLoop()
+    }
+  }
+  doCommandLoop()
+
+
 
 }
