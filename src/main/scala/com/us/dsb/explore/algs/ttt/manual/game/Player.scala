57d363d25306120452a24586ab71edd571079cfd
Daniel Barclay <daniel.barclay@qomplx.com>
2022-01-25 14:19:56 -0500

    ManualTicTacToe:  Shrunk ~static getOtherPlayer to member otherPlayer.

--- src/main/scala/com/us/dsb/explore/algs/ttt/manual/game/Player.scala
+++ src/main/scala/com/us/dsb/explore/algs/ttt/manual/game/Player.scala
@@ -3,17 +3,9 @@ package com.us.dsb.explore.algs.ttt.manual.game
 import enumeratum.EnumEntry
 
 /** Player identity. */
-sealed trait Player extends EnumEntry
+sealed class Player(val otherPlayer: Player) extends EnumEntry
 private[game] object Player {
   /** Player who goes first (per traditional rules/naming). */
-  private[game] case object X extends Player
-  private[game] case object O extends Player
-
-  private[game] def getOtherPlayer(`given`: Player): Player = {
-     `given` match {
-       case X => O
-       case O => X
-     }
-   }
-
+  private[game] case object X extends Player(O)
+  private[game] case object O extends Player(X)
 }
