2ae601ca747fe9d58c14d414bfdb5bb31b75b7db
Daniel Barclay <daniel.barclay@qomplx.com>
2022-02-12 22:32:32 -0500

    DebuggingDetector:  Adjusted.

--- src/main/scala/com/us/dsb/explore/DebuggingDetector.scala
+++ src/main/scala/com/us/dsb/explore/DebuggingDetector.scala
@@ -5,7 +5,9 @@ import scala.jdk.CollectionConverters._
 object DebuggingDetector extends App {
   val bean = java.lang.management.ManagementFactory.getRuntimeMXBean
   val jvmArgs = bean.getInputArguments.asScala
-  println(jvmArgs.mkString("JVM args:\n- ", "\n- ", ""))
-  val startedWithDebugger = jvmArgs.exists(_.startsWith("-agentlib:jdwp"))
+  println("DebuggingDetector:  " + jvmArgs.mkString("JVM args:\n- ", "\n- ", ""))
+  val startedWithDebugger =
+    jvmArgs.exists {
+      arg => arg.startsWith("-agentlib:jdwp") || arg.startsWith("-Xrunjdwp")}
   println("DebuggingDetector:  startedWithDebugger = " + startedWithDebugger)
 }
