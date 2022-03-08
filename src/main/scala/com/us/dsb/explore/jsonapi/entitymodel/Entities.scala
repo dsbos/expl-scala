bbbf5a555c6575b45b36fbfec1b060e5ab2e5a02
Daniel Barclay <daniel.barclay@qomplx.com>
2022-03-08 11:37:58 -0500

    JSON:API-related modeling:  N levels for attribute types.

--- src/main/scala/com/us/dsb/explore/jsonapi/entitymodel/Entities.scala
+++ src/main/scala/com/us/dsb/explore/jsonapi/entitymodel/Entities.scala
@@ -3,130 +3,143 @@ package com.us.dsb.explore.jsonapi.entitymodel
 import com.us.dsb.explore.jsonapi.entitymodel
 
 
-trait AttributeTypeNEW
-
-
-trait PrimitiveType {
-  val name: String  // PrimitiveTypeName?
-}
-object PrimitiveType {
-  class /*??*/NameThis1(val name: String)
-  case object StringType extends NameThis1("string")
-}
+// ?? Maybe -> DataType if not just for entity attribute types.  (But in filter
+//   expressions, still closely related to attribute types.)
 
 trait AttributeType {
   val name: String  // AttributeTypeName?
-  //val baseType: PrimitiveType //???? generalize, to allow subtyping
-}
-object AttributeType {
-  class /*??*/NameThis2(val name: String) extends AttributeType
-  case object UserName extends NameThis2("userName")
-
+  // maybe JSON representation type, or codecs
+  // maybe Scala representation type
 }
 
-
-trait Attribute_Sharable {
-  val name: String  // FieldName?
-  val `type`: AttributeType
-}
-object Attribute_Sharable {
-  class /*??*/NameThis3(val name: String, val `type`: AttributeType) extends Attribute_Sharable
-  def apply(name: String, `type`: AttributeType): Attribute_Sharable =
-    new /*??*/NameThis3(name, `type`)
-
-  // ??? What about String -> EntityName -> UserName?
-  case object EntityName extends NameThis3("name", AttributeType.UserName)
+class PrimitiveType(val name: String)  extends AttributeType {
+  // anything specific to primitive type?
 }
 
-trait Attribute_Owned {
-  val parentEntity: Entity
-  val name: String  // FieldName?
-  val `type`: AttributeType
-}
-object Attribute_Owned {
-  class /*??*/NameThis3b(val parentEntity: Entity,
-                   val name: String,
-                   val `type`: AttributeType) extends Attribute_Owned
-  def apply(parentEntity: Entity, name: String, `type`: AttributeType): Attribute_Owned =
-    new /*??*/NameThis3b(parentEntity, name, `type`)
+// ?? Where will enumeration types fit in?
 
-  //// ??? What about String -> EntityName -> UserName?
-  //case object EntityName extends NameThis3b("name", AttributeType.UserName)
-}
-
-
-
-trait Relationship {
-  val name: String
-  val otherEntityType: Entity
-
-}
-
-/**
- * ...
- * @param singularIdentifer ...; e.g., adminUser    (JSON:API type)
- * @param pluralIdentifer   ...; e.g., adminUsers   (JSON:API URL segment)
- * @param singularLabel     ...; e.g., Admin. User  (single-thing UI label)
- * @param pluralLabel       ...; e.g., Admin. Users (things-list UI label)
- * @param singularPhrase    ...; e.g., admin. user  (single-thing message)
- * @param pluralPhrase      ...; e.g., admin. users (multiple-things messages)
- * @param other             e.g., description of entity kind, tool-tips, etc.
- */
-case class EntityTypeNameInfo(
-  singularIdentifer: String,
-  pluralIdentifer: String,
-  singularLabel: String,
-  pluralLabel: String,
-  singularPhrase: String,
-  pluralPhrase: String,
-  other: String
-  )
-object EntityTypeNameInfo {
-  /** Constructs with plurals made by by appending "s". */
-  def apply(singularIdentifer: String,
-            singularLabel: String,
-            singularPhrase: String,
-            other: String): EntityTypeNameInfo =
-    EntityTypeNameInfo(singularIdentifer,
-                       singularIdentifer + "s",
-                       singularLabel,
-                       singularLabel + "s",
-                       singularPhrase,
-                       singularPhrase + "s",
-                       other)
-  "".toLowerCase
-
-}
-
-trait Entity {
-  val `type`: EntityTypeNameInfo
-  val attributes_1: Set[Attribute_Sharable]
-  val attributes_2: Set[Attribute_Owned]
-  val relationships: Set[Relationship]
-}
-
-trait EntityTableColumn
-trait EntityTable
-
-
-
-object UserEntity extends Entity {
-  val `type`: EntityTypeNameInfo =
-    EntityTypeNameInfo("adUser", "AD User", "AD user", "...")
-  object Attributes {
-    val name_1 = Attribute_Sharable.EntityName
-    case object name_2 extends entitymodel.Attribute_Owned.NameThis3b(UserEntity, "name", AttributeType.UserName)
+class DerivedType(val name: String,
+                  val baseType: AttributeType
+                 ) extends AttributeType {
+  val rootType: AttributeType = {
+    baseType match {
+      case p: PrimitiveType => p
+      case d: DerivedType => d.baseType
+    }
   }
-  val attributes_1: Set[Attribute_Sharable] =
-    Set(Attributes.name_1
-    )
-  val attributes_2: Set[Attribute_Owned] =
-    Set(Attributes.name_2
-    )
-  val relationships: Set[Relationship] = Set()
-
 }
 
-//object DomainEntity extends Entity
+object AttributeType {
+  case object BooleanType extends PrimitiveType("boolean")
+  case object StringType extends PrimitiveType("string")
+  case object EntityName extends DerivedType("entityName", StringType)
+  case object UserName   extends DerivedType("userName", EntityName)
+  // (even "case object AdminUserName extends DerivedType("adminUserName", UserName)" )
+}
 
 
+//trait xxAttribute_Sharable {
+//  val name: String  // FieldName?
+//  val `type`: AttributeType
+//}
+//object xxAttribute_Sharable {
+//  class /*??*/NameThis3(val name: String, val `type`: AttributeType) extends xxAttribute_Sharable
+//  def apply(name: String, `type`: AttributeType): xxAttribute_Sharable =
+//    new /*??*/NameThis3(name, `type`)
+//
+//  // ??? What about String -> EntityName -> UserName?
+//  case object xxEntityName extends NameThis3("name", xxAttributeTypeCHOOSE.xxUserName)
+//}
+//
+//trait xxAttribute_Owned {
+//  val parentEntity: xxEntity
+//  val name: String  // FieldName?
+//  val `type`: AttributeType
+//}
+//object xxAttribute_Owned {
+//  class /*??*/NameThis3b(val parentEntity: xxEntity,
+//                   val name: String,
+//                   val `type`: xxAttributeTypeCHOOSE) extends xxAttribute_Owned
+//  def apply(parentEntity: xxEntity, name: String, `type`: xxAttributeTypeCHOOSE): xxAttribute_Owned =
+//    new /*??*/NameThis3b(parentEntity, name, `type`)
+//
+//  //// ??? What about String -> EntityName -> UserName?
+//  //case object xxEntityName extends NameThis3b("name", AttributeTypeCHOOSE.UserName)
+//}
+//
+//
+//
+//trait xxRelationship {
+//  val name: String
+//  val otherEntityType: xxEntity
+//
+//}
+//
+///**
+// * ...
+// * @param singularIdentifer ...; e.g., adminUser    (JSON:API type)
+// * @param pluralIdentifer   ...; e.g., adminUsers   (JSON:API URL segment)
+// * @param singularLabel     ...; e.g., Admin. User  (single-thing UI label)
+// * @param pluralLabel       ...; e.g., Admin. Users (things-list UI label)
+// * @param singularPhrase    ...; e.g., admin. user  (single-thing message)
+// * @param pluralPhrase      ...; e.g., admin. users (multiple-things messages)
+// * @param other             e.g., description of entity kind, tool-tips, etc.
+// */
+//case class xxEntityTypeNameInfo(
+//  singularIdentifer: String,
+//  pluralIdentifer: String,
+//  singularLabel: String,
+//  pluralLabel: String,
+//  singularPhrase: String,
+//  pluralPhrase: String,
+//  other: String
+//  )
+//object xxEntityTypeNameInfo {
+//  /** Constructs with plurals made by by appending "s". */
+//  def apply(singularIdentifer: String,
+//            singularLabel: String,
+//            singularPhrase: String,
+//            other: String): xxEntityTypeNameInfo =
+//    xxEntityTypeNameInfo(singularIdentifer,
+//                       singularIdentifer + "s",
+//                       singularLabel,
+//                       singularLabel + "s",
+//                       singularPhrase,
+//                       singularPhrase + "s",
+//                       other)
+//  "".toLowerCase
+//
+//}
+//
+//trait xxEntity {
+//  val `type`: xxEntityTypeNameInfo
+//  val attributes_1: Set[xxAttribute_Sharable]
+//  val attributes_2: Set[xxAttribute_Owned]
+//  val relationships: Set[xxRelationship]
+//}
+//
+//trait xxEntityTableColumn
+//trait xxEntityTable
+//
+//
+//
+//object xxUserEntity extends xxEntity {
+//  val `type`: xxEntityTypeNameInfo =
+//    xxEntityTypeNameInfo("adUser", "AD User", "AD user", "...")
+//  object xxAttributes {
+//    val name_1 = xxAttribute_Sharable.xxEntityName
+//    case object xxname_2 extends xxAttribute_Owned.NameThis3b(xxUserEntity, "name", xxAttributeTypeCHOOSE.xxUserName)
+//  }
+//  val attributes_1: Set[xxAttribute_Sharable] =
+//    Set(xxAttributes.name_1
+//    )
+//  val attributes_2: Set[xxAttribute_Owned] =
+//    Set(xxAttributes.xxname_2
+//    )
+//  val relationships: Set[xxRelationship] = Set()
+//
+//}
+//
+////object xxDomainEntity extends xxEntity
+//
+//
