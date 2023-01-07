535aa2d86feeb1a2b1f0a495c86128e9e48fdd0b
Daniel Barclay <daniel.barclay@qomplx.com>
2023-01-07 15:03:47 -0500

    ColoredLines:  Tests:  LineDetector.haveMatchingBallAt

--- src/test/scala/com/us/dsb/explore/algs/coloredlines/manual/game/lines/LineDetectorTest.scala
+++ src/test/scala/com/us/dsb/explore/algs/coloredlines/manual/game/lines/LineDetectorTest.scala
@@ -3,6 +3,10 @@ package com.us.dsb.explore.algs.coloredlines.manual.game.lines
 import com.us.dsb.explore.algs.coloredlines.manual.game.Board.CellAddress
 import com.us.dsb.explore.algs.coloredlines.manual.game.{BallKind, Board, BoardOrder}
 import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.PrivateMethodTester
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers._
+
 
 import scala.reflect.internal.util.TableDef.Column
 import scala.tools.nsc.doc.base.comment.Cell
@@ -17,20 +21,33 @@ class LineDetectorTest extends AnyFunSpec {
     it("anything worth testing?") {}
   }
 
-  describe("haveMatchingBallAt") {
-    ignore/*it*/("TBD") {}
-//    lazy val sampleBoard =
-//      Board.empty
-//          .withBallAt()
-//
-//    it("TBD") {
-//      LineDetector.haveMatchingBallAt(moveBallColor: BallKind,
-//                                      board        : Board,
-//                                      rawRowIndex  : Int,
-//                                      rawColIndex  : Int)
-//
-//
-//    }
+  describe("haveMatchingBallAt:") {
+    lazy val `R1/C1-only color` = BallKind.values(0)
+    lazy val `R2/C2-only color` = BallKind.values(1)
+    lazy val sampleBoard =
+      Board.empty
+          .withBallAt(CellAddress.fromRaw(1, 1), `R1/C1-only color`)
+          .withBallAt(CellAddress.fromRaw(2, 2), `R2/C2-only color`)
+    it("should return true for good match (valid index, non-empty cell, same ball color)") {
+      LineDetector.haveMatchingBallAt(`R1/C1-only color`, sampleBoard, 1, 1) shouldBe true
+      LineDetector.haveMatchingBallAt(`R2/C2-only color`, sampleBoard, 2, 2) shouldBe true
+    }
+    it("should return false for different color") {
+      LineDetector.haveMatchingBallAt(`R2/C2-only color`, sampleBoard, 1, 1) shouldBe false
+      LineDetector.haveMatchingBallAt(`R1/C1-only color`, sampleBoard, 2, 2) shouldBe false
+    }
+    it("should return false for empty cell") {
+      LineDetector.haveMatchingBallAt(`R2/C2-only color`, sampleBoard, 1, 2) shouldBe false
+      LineDetector.haveMatchingBallAt(`R2/C2-only color`, sampleBoard, 2, 1) shouldBe false
+      LineDetector.haveMatchingBallAt(`R2/C2-only color`, sampleBoard, 3, 3) shouldBe false
+      LineDetector.haveMatchingBallAt(`R1/C1-only color`, sampleBoard, 3, 3) shouldBe false
+    }
+    it("should return false for bad coordinates") {
+      LineDetector.haveMatchingBallAt(`R1/C1-only color`, sampleBoard, -1,             1 ) shouldBe false
+      LineDetector.haveMatchingBallAt(`R1/C1-only color`, sampleBoard, 1,              -2) shouldBe false
+      LineDetector.haveMatchingBallAt(`R1/C1-only color`, sampleBoard, BoardOrder + 3, 1 ) shouldBe false
+      LineDetector.haveMatchingBallAt(`R1/C1-only color`, sampleBoard, 1,              BoardOrder + 1) shouldBe false
+    }
   }
 
   describe("RelativeDirectionResult") {
